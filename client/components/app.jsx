71de62095fb34f139c15c930f956997b4bc5a0b3
UnaJeon <unajeon@gmail.com>
2020-07-17 00:13:01 +0000

    gave initail state for grades and defined a Header component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [] };
+  }
+
   render() {
-    return null;
+    return (
+      <Header />
+    );
   }
 }
 
+function Header() {
+  return (
+    <h1>Student Grade Table</h1>
+  );
+}
 export default App;
